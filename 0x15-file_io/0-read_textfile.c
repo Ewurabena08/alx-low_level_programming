41c2bb6903e2e851a666c632c9e5b1a36eb0177c
Ewurabena08 <estherawuah44@gmail.com>
2023-08-10 08:48:55 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 /**
- * a function that reads a text file and prints it to the POSIX standard output.
  * @letters: the number of letters which are to be read
  * read_textfile- Read a text file to be printed to STDOUT.
  * @filename: text file being read
