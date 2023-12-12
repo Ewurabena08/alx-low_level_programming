ce77e33d7a8b036fd4c6b2a5d73708eebe626d25
Ewurabena08 <estherawuah44@gmail.com>
2023-12-11 16:14:38 -0800

    hacking

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}	
+/**
+ * mod - mod two integers
+ * @a: first int
+ * @b: second int
+ * Return: mode
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}
+
+/**
+ * mul- multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * sub -subract two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}	
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sub -subract two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtraction
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}	
